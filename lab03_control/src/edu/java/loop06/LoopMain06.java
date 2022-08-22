ddabcb3072453368d6c833318e987386260a2272
Jake Oh <jake.oh.lecture@gmail.com>
2022-08-22 12:18:48 +0900

    if, for, while

---
+++ lab03_control/src/edu/java/loop06/LoopMain06.java
@@ -0,0 +1,21 @@
+package edu.java.loop06;
+
+public class LoopMain06 {
+
+    public static void main(String[] args) {
+        // for 반복문
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+        
+        System.out.println("-----");
+        // while 반복문
+        int n = 1;
+        while (n <= 5) {
+            System.out.println(n);
+            n++;
+        }
+        
+    }
+
+}
