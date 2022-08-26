544150905492bf6998649056dcf269aaefbf7b59
Jake Oh <jake.oh.lecture@gmail.com>
2022-08-26 14:19:57 +0900

    OOP

---
+++ lab06_class/src/edu/java/class01/ClassMain01.java
@@ -0,0 +1,15 @@
+package edu.java.class01;
+
+public class ClassMain01 {
+
+    public static void main(String[] args) {
+        // User 클래스 타입으로 변수 선언, 객체 생성, 초기화.
+        User user1 = new User();
+        System.out.println(user1);
+        System.out.println(user1.userId);
+        System.out.println(user1.password);
+        System.out.println(user1.age);
+
+    }
+
+}
---
+++ lab06_class/src/edu/java/class01/User.java
@@ -0,0 +1,10 @@
+package edu.java.class01;
+
+// 클래스: 데이터 + 기능 ==> 데이터 타입
+public class User {
+    
+    String userId;
+    String password;
+    int age;
+
+}
