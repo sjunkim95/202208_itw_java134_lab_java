87ec8255177e548f5a075159f1a52e146adb07cf
Jake Oh <jake.oh.lecture@gmail.com>
2022-08-29 17:33:12 +0900

    OOP

---
+++ lab06_class/src/edu/java/class06/Account.java
@@ -0,0 +1,43 @@
+package edu.java.class06;
+
+/**
+ * 은행 계좌 정보 클래스.
+ * 계좌번호, 잔액을 필드로 갖고, 입금, 출금, 이체, 정보 출력 기능을 메서드로 갖음.
+ */
+public class Account {
+    // field
+    int accountNo; // 계좌번호
+    double balance; // 잔고
+    
+    // constructor - argument 2개를 갖는 생성자.
+
+    /**
+     * 입금(deposit)
+     * 
+     * @param amount 입금액
+     * @return 입금 후 잔액을 리턴.
+     */
+    
+    
+    /**
+     * 출금(withdraw)
+     * 
+     * @param amount 출금액
+     * @return 출금 후 잔액을 리턴.
+     */
+    
+    
+    /**
+     * 이체(transfer)
+     * 
+     * @param to 이체할 계좌 객체(Account 타입)
+     * @param amount 이체할 금액
+     * @return true(이체 성공).
+     */
+    
+    
+    /**
+     * 계좌 정보 출력(printAccountInfo). 계좌 번호와 잔액을 출력.
+     */
+    
+}
