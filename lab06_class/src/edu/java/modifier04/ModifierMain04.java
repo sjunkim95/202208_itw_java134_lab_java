9fc88b180855ab4f62159914020001aa095b3ca3
Jake Oh <jake.oh.lecture@gmail.com>
2022-08-30 15:22:18 +0900

    modifier

--- lab06_class/src/edu/java/modifier04/ModifierMain04.java
+++ lab06_class/src/edu/java/modifier04/ModifierMain04.java
@@ -8,6 +8,7 @@ public class ModifierMain04 {
         System.out.println(m);
         System.out.println(m.getMemberId());
 //        m.memberId = ""; // private 멤버는 다른 클래스에서 직접 사용할 수 없음.
+        // private 멤버 변수의 setter 메서드를 제공하지 않기 때문에 값을 변경할 수 없음.
         System.out.println(m.getPassword());
         m.setPassword("!@#$");
         System.out.println(m.getPassword());
---
+++ lab06_class/src/edu/java/modifier07/ModifierMain07.java
@@ -0,0 +1,25 @@
+package edu.java.modifier07;
+
+// 객체(object): 프로그램에서 만들 대상. (비유) 붕어빵
+// 클래스(class): 객체를 생성하기 위한 설계도. 데이터 타입. (비유) 붕어빵 틀
+// 인스턴스(instance): 메모리에 생성된 객체. (비유) 만들어진 붕어빵.
+
+// 인스턴스 멤버(변수/필드, 메서드)
+// - static 수식어가 사용되지 않은 멤버.
+// - 객체를 생성한 후에, 참조 변수를 이용해서 사용.
+// - Java Runtime이 사용하는 메모리 공간 중 Heap(힙) 영역에 생성됨.
+
+// static 멤버(변수/필드, 메서드)
+// - static 수식어가 사용된 멤버.
+// - 객체를 생성하지 않아도 사용할 수 있는 멤버.
+// - 클래스 이름을 이용해서 사용.
+// - Java Runtime이 사용하는 메모리 공간 중 Method(메서드) 영역에 생성됨.
+
+public class ModifierMain07 {
+
+    public static void main(String[] args) {
+        
+
+    }
+
+}
