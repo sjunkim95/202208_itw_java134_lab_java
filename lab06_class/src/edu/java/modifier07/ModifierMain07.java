17c090b95002a8f707fa35d328578762ab807a41
Jake Oh <jake.oh.lecture@gmail.com>
2022-08-30 16:23:45 +0900

    static

--- lab06_class/src/edu/java/modifier07/ModifierMain07.java
+++ lab06_class/src/edu/java/modifier07/ModifierMain07.java
@@ -12,14 +12,33 @@ package edu.java.modifier07;
 // static 멤버(변수/필드, 메서드)
 // - static 수식어가 사용된 멤버.
 // - 객체를 생성하지 않아도 사용할 수 있는 멤버.
-// - 클래스 이름을 이용해서 사용.
+// - 클래스 이름을 이용해서 사용. (예) Math.PI, Math.sqrt()
 // - Java Runtime이 사용하는 메모리 공간 중 Method(메서드) 영역에 생성됨.
+// - 프로그램 로딩 시점(main 메서드 시작 전)에 메모리에 생겨남.
+// - static 멤버들은 static 멤버들만 접근(사용) 가능.
 
 public class ModifierMain07 {
 
     public static void main(String[] args) {
+        // static 멤버(변수, 메서드)는 클래스 이름으로 참조해서 사용.
+        System.out.println(StaticTest.staticVariable);
+        StaticTest.staticVariable = 100;
+        StaticTest.printVariables2();
         
+        System.out.println();
+        // 인스턴스 멤버(변수, 메서드)는 객체 생성 후 참조 변수를 이용해서 사용.
+        StaticTest test1 = new StaticTest();
+        System.out.println(test1.instanceVariable);
+        System.out.println(test1.staticVariable);
+        // static 변수를 참조 변수를 이용해서 사용할 수는 있지만, 권장하지 않는 방법.
+        // static 변수는 클래스 이름으로 참조하는 것을 권장.
+        
+        test1.instanceVariable = 10;
+        test1.printVariables();
 
+        StaticTest test2 = new StaticTest();
+        test2.printVariables();
+        
     }
 
 }
