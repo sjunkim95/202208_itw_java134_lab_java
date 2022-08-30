809edd009970d6fce030918d333bdd7ff982b79e
Jake Oh <jake.oh.lecture@gmail.com>
2022-08-30 10:32:01 +0900

    OOP

--- lab06_class/src/edu/java/class06/Account.java
+++ lab06_class/src/edu/java/class06/Account.java
@@ -10,34 +10,60 @@ public class Account {
     double balance; // 잔고
     
     // constructor - argument 2개를 갖는 생성자.
+    public Account(int accountNo, double balance) {
+        this.accountNo = accountNo;
+        this.balance = balance;
+    }
 
     /**
      * 입금(deposit)
      * 
-     * @param amount 입금액
+     * @param amount 입금액.
      * @return 입금 후 잔액을 리턴.
      */
+    public double deposit(double amount) {
+        this.balance += amount; // this.balance = this.balance + amount
+        return this.balance;
+    }
     
     
     /**
      * 출금(withdraw)
      * 
-     * @param amount 출금액
+     * @param amount 출금액.
      * @return 출금 후 잔액을 리턴.
      */
+    public double withdraw(double amount) {
+        this.balance -= amount;
+        return this.balance;
+    }
     
     
     /**
      * 이체(transfer)
      * 
-     * @param to 이체할 계좌 객체(Account 타입)
-     * @param amount 이체할 금액
+     * @param to 이체할 계좌 객체(Account 타입).
+     * @param amount 이체할 금액.
      * @return true(이체 성공).
      */
+    public boolean transfer(Account to, double amount) {
+        // 이체: (1) 내 계좌 출금. (2) 상대방 계좌 입금.
+//        this.balance -= amount;
+//        to.balance += amount;
+        this.withdraw(amount);
+        to.deposit(amount);
+        
+        return true;
+    }
     
     
     /**
      * 계좌 정보 출력(printAccountInfo). 계좌 번호와 잔액을 출력.
      */
+    public void printAccountInfo() {
+        System.out.println("--- 계좌 정보 ---");
+        System.out.println("계좌번호: " + this.accountNo);
+        System.out.println("잔고: " + this.balance);
+    }
     
 }
