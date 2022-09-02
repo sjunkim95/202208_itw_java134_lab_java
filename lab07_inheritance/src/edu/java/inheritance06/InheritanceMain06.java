9f25c1d7aea2a82a681dc39f482cedc912b9b3b6
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-02 10:18:46 +0900

    inheritance & final

---
+++ lab07_inheritance/src/edu/java/inheritance06/InheritanceMain06.java
@@ -0,0 +1,16 @@
+package edu.java.inheritance06;
+
+public class InheritanceMain06 {
+
+    public static void main(String[] args) {
+        // java.lang.Object 클래스: 자바의 최상위 클래스
+        // -> 자바의 모든 클래스 Object 클래스의 public 멤버(변수, 메서드)를 사용할 수 있음.
+        T t = new T();
+        
+        
+    }
+
+}
+
+class T {}
+class U {}
