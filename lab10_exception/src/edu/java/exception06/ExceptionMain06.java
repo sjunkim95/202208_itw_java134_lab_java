9849905415414395d5f12176a9052b829c5c7ed6
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-07 16:27:42 +0900

    exception

--- lab10_exception/src/edu/java/exception06/ExceptionMain06.java
+++ lab10_exception/src/edu/java/exception06/ExceptionMain06.java
@@ -7,10 +7,16 @@ public class ExceptionMain06 {
         
         try {
             int result = calc.divide(100, 0);
+            //-> 반드시 처리를 해야 하는 exception을 throws한다고 선언된 메서드를 호출할 때는 
+            // try-catch 구문을 사용하거나 throws 선언문을 메서드 선언에 추가해야 함.
             System.out.println(result);
-        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
+//            e.printStackTrace();
+            // 예외가 발생한 이유와 예외가 발생되기 까지의 코드 호출 순서를 출력해줌.
             System.out.println(e.getMessage());
         }
+        
+        System.out.println("정상 종료");
     }
 
 }
---
+++ lab11_api/src/edu/java/string01/Ex01.java
@@ -0,0 +1,27 @@
+package edu.java.string01;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+        // String 클래스의 메서드들을 사용
+        
+        // ex-1. 아래의 주민번호 문자열에서 성별을 표시하는 위치의 문자만 출력.
+        String ssn = "991231-1234567";
+        // TODO
+        
+        // ex-2. 아래의 문자열 배열에서 "홍길동"의 인덱스를 출력.
+        // 만약에 "홍길동"이 배열에 없는 경우에는 -1을 출력.
+        String[] names = {"오쌤", "홍길동", "John", "Jane"};
+        // TODO
+        
+        // ex-3. 아래의 문자열 배열에서 5글자 이상인 문자열들만 출력.
+        String[] languages = {"Java", "SQL", "JavaScript", "Python"};
+        // TODO
+        
+        // ex-4. 아래의 문자열 배열에서 대소문자 구별없이 "est"를 포함하는 문자열들을 출력.
+        String[] tests = {"TEST", "test", "TeSt", "tEsT", "테스트"};
+        // TODO
+
+    }
+
+}
