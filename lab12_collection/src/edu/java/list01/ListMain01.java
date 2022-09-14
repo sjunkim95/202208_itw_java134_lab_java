e6aa4d6a1a520092aa5f0e7ca0dbc3583259041b
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-14 17:21:50 +0900

    List

--- lab12_collection/src/edu/java/list01/ListMain01.java
+++ lab12_collection/src/edu/java/list01/ListMain01.java
@@ -1,6 +1,7 @@
 package edu.java.list01;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /*
  * Collection<E>
@@ -42,11 +43,44 @@ public class ListMain01 {
         System.out.println(languages);
         
         languages.add("SQL");
+        languages.add("HTML/CSS/JavaScript");
+        languages.add("Servlet/JSP");
+        languages.add("Spring");
+        languages.add("Java");
         System.out.println(languages);
         
         // get(int index): 리스트에서 index 위치의 원소(E)를 리턴.
         System.out.println(languages.get(0));
         System.out.println(languages.get(1));
+        
+        // remove(Object o): 리스트에서 처음 등장하는 element 1개를 삭제.
+        languages.remove("Java");
+        System.out.println(languages);
+        
+        // remove(int index): 리스트에서 index 위치에 있는 원소를 삭제.
+        languages.remove(1);
+        System.out.println(languages);
+        
+        // 반복문을 사용한 리스트 원소 출력
+        // 1) index 이용
+        for (int i = 0; i < languages.size(); i++) {
+            System.out.print(languages.get(i) + ", ");
+        }
+        System.out.println();
+        
+        // 2) for-each 문장 이용
+        for (String s : languages) {
+            System.out.print(s + ", ");
+        }
+        System.out.println();
+        
+        // 3) Iterator<E> 이용
+        Iterator<String> itr = languages.iterator(); // 리스트 원소를 순회(iterate)하는 도구를 생성.
+        while (itr.hasNext()) { // 리스트에서 다음 원소가 존재하면
+            // next(): 현재 원소를 리턴하고 위치를 다음 위치로 이동.
+            System.out.print(itr.next() + ", ");
+        }
+        System.out.println();
         
     }
 
---
+++ lab12_collection/src/edu/java/list02/ListMain02.java
@@ -0,0 +1,26 @@
+package edu.java.list02;
+
+public class ListMain02 {
+
+    public static void main(String[] args) {
+        // 1. 정수를 저장하는 LinkedList 변수(numbers) 선언, 객체 생성.
+        
+        // 2. 리스트에 [0, 100) 범위의 정수 난수 20개를 저장.
+        
+        // 3. 난수가 저장된 리스트(numbers)를 출력.
+        
+        // 4. 홀수들만 저장하는 LinkedList 변수(odds) 선언, 객체 생성.
+        
+        // 5. 난수들이 저장된 리스트 numbers에서 홀수들만 리스트 odds에 저장.
+        
+        // 6. 리스트 odds를 출력.
+        
+        // 7. 짝수 제곱들을 저장하는 LinkedList 변수(evenSquares) 선언, 객체 생성.
+        
+        // 8. 난수들이 저장된 리스트 numbers에서 짝수들을 찾아서 그 제곱을 evenSquares에 저장.
+        
+        // 9. 리스트 evenSquares를 출력.
+
+    }
+
+}
