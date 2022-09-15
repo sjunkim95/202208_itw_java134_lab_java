16ebca1a04e6a2ef09ae03f87a5a13fffb5cde70
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-15 14:47:06 +0900

    List

--- lab12_collection/src/edu/java/list04/User.java
+++ lab12_collection/src/edu/java/list04/User.java
@@ -5,7 +5,32 @@ public class User {
     private String password;
     
     // 생성자: (1) 기본 생성자, (2) argument 2개를 갖는 생성자
+    public User() {}
+
+    public User(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
     
     // getters/setters
+    public String getUserId() {
+        return userId;
+    }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User(userId=%s, password=%s)", userId, password);
+    }
 }
