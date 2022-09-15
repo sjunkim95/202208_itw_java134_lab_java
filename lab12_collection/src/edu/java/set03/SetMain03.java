3ff60af9e7d0ff56487a1c7efe5a2eea05523817
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-15 17:13:50 +0900

    Set

---
+++ lab12_collection/src/edu/java/set03/SetMain03.java
@@ -0,0 +1,12 @@
+package edu.java.set03;
+
+public class SetMain03 {
+
+    public static void main(String[] args) {
+        // HashSet<E> 사용.
+        // [0, 10) 범위의 정수 난수 5개를 저장.
+        // Set의 내용을 출력.
+
+    }
+
+}
