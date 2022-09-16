5fcbd6b22b836bf8d805890158f11e651316d626
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-16 09:50:09 +0900

    Set

--- lab12_collection/src/edu/java/set03/SetMain03.java
+++ lab12_collection/src/edu/java/set03/SetMain03.java
@@ -1,12 +1,29 @@
 package edu.java.set03;
 
+import java.util.HashSet;
+import java.util.Random;
+
 public class SetMain03 {
 
     public static void main(String[] args) {
         // HashSet<E> 사용.
         // [0, 10) 범위의 정수 난수 5개를 저장.
         // Set의 내용을 출력.
-
+        HashSet<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+        
+        while (true) {
+            int r = random.nextInt(10); // 난수 만듦.
+            System.out.println("r = " + r);
+            
+            numbers.add(r); // 난수를 set에 저장.
+            System.out.println("set = " + numbers);
+            
+            if (numbers.size() == 5) { // set의 원소 개수가 5개이면
+                break; // 반복문 멈춤.
+            }
+        }
+        
     }
 
 }
