8d12655ef0316c896e0c570f7410b9931d27ded7
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-16 11:32:26 +0900

    Map

---
+++ lab12_collection/src/edu/java/map03/MapMain03.java
@@ -0,0 +1,15 @@
+package edu.java.map03;
+
+public class MapMain03 {
+
+    public static void main(String[] args) {
+        // 단어 개수 세기(word counting)
+        String sentence = "하늘 바다 땅 하늘 땅 사람";
+        
+        // 문자열 sentence에 등장하는 단어를 key로 하고 
+        // 그 단어가 문자열에 등장하는 횟수를 value로 하는 Map 객체를 만들고 출력.
+        // 결과: {땅=2, 바다=1, 사람=1, 하늘=2}
+
+    }
+
+}
