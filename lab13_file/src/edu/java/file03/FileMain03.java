31edb2d892154e23370cfaea6be088c7c215bcc8
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-19 16:32:55 +0900

    file

---
+++ lab13_file/src/edu/java/file03/FileMain03.java
@@ -0,0 +1,13 @@
+package edu.java.file03;
+
+public class FileMain03 {
+
+    public static void main(String[] args) {
+        // data/ratings.data 파일을 읽고  ratings_copy.data 파일에 복사.
+        // read(), write(int b)를 사용했을 때와
+        // read(byte[] b), write(byte[] b, int off, int len)를 사용했을 때의
+        // 복사 시간의 차이를 비교.
+
+    }
+
+}
