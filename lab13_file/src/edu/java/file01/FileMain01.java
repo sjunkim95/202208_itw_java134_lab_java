d008963c2b04aeb6da61fc519f3e740c94eae7a3
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-19 11:24:46 +0900

    file

---
+++ lab13_file/src/edu/java/file01/FileMain01.java
@@ -0,0 +1,17 @@
+package edu.java.file01;
+
+/*
+ * 입/출력 스트림(Input/Output Stream): 프로그램이 값(데이터)를 입력받거나 출력하는 통로.
+ * 프로그램 <== InputStream <== 입력 장치(키보드, 마우스, 터치스크린, 펜, 파일, ...)
+ * 프로그램 ==> OutputStream ==> 출력 장치(모니터, 프린터, 스크린, 파일, ...)
+ * 
+ */
+
+public class FileMain01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
