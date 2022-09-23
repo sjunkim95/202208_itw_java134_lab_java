c1c63bdfd561fbdd19894e086af7773df1af2534
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-23 14:22:30 +0900

    lambda

--- lab14_inner/src/edu/java/lambda03/LambdaMain03.java
+++ lab14_inner/src/edu/java/lambda03/LambdaMain03.java
@@ -27,8 +27,53 @@ public class LambdaMain03 {
         System.out.println(evens2);
         
         // numbers에서 홀수들만 선택해서 저장한 리스트.
-        List<Integer> odds; 
+        List<Integer> odds = new ArrayList<>();
+        for (Integer x : numbers) {
+            if (x % 2 == 1) {
+                odds.add(x);
+            }
+        }
+        System.out.println(odds);
+        
+        List<Integer> odds2 = numbers.stream()
+                .filter(x -> x % 2 == 1)
+                .collect(Collectors.toList());
+        System.out.println(odds2);
+        
+        // numbers에서 짝수들의 제곱을 저장하는 리스트.
+        List<Integer> result = new ArrayList<>();
+        for (Integer x : numbers) {
+            if (x % 2 == 0) {
+                result.add(x * x);
+            }
+        }
+        System.out.println(result);
+        
+        result = numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * x)
+                .collect(Collectors.toList());
+        System.out.println(result);
 
+        List<String> langs = Arrays.asList("Java", "SQL", "HTML", "JavaScript", "Python");
+        System.out.println(langs);
+        
+        // langs에서 5글자 이상의 문자열들의 소문자로 변환한 문자열을 갖는 리스트.
+        // [javascript, python]
+        List<String> longer = new ArrayList<>();
+        for (String x : langs) {
+            if (x.length() >= 5) {
+                longer.add(x.toLowerCase());
+            }
+        }
+        System.out.println(longer);
+        
+        longer = langs.stream()
+                .filter(x -> x.length() >= 5)
+                .map(String::toLowerCase) // .map(x -> x.toLowerCase())
+                .collect(Collectors.toList());
+        System.out.println(longer);
+        
     }
 
 }
