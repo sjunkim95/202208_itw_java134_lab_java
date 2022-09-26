694b8a799cf50f13adc301e407f533ddce6e2224
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-26 12:15:37 +0900

    swing

---
+++ lab16_swing/src/edu/java/swing01/AppMain01.java
@@ -0,0 +1,52 @@
+package edu.java.swing01;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class AppMain01 {
+
+    private JFrame frame;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    AppMain01 window = new AppMain01();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public AppMain01() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(200, 200, 600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JLabel lblText = new JLabel("안녕하세요, Swing!"); // JLabel 객체 생성
+        lblText.setHorizontalAlignment(SwingConstants.CENTER);
+        lblText.setFont(new Font("D2Coding", Font.PLAIN, 40));
+        frame.getContentPane().add(lblText, BorderLayout.CENTER); // JLabel 객체를 화면에 추가.
+    }
+
+}
