80fd0352b6e206b3781886a076a4b6a788dfcbf4
Jake Oh <jake.oh.lecture@gmail.com>
2022-09-27 11:25:08 +0900

    swing

---
+++ lab16_swing/src/edu/java/swing06/AppMain06.java
@@ -0,0 +1,88 @@
+package edu.java.swing06;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class AppMain06 {
+
+    private JFrame frame;
+    private JButton btnConfirmDlg;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    AppMain06 window = new AppMain06();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public AppMain06() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 480);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        
+        JButton btnMsgDlg = new JButton("Message Dialog");
+        btnMsgDlg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                JOptionPane.showMessageDialog(frame, "안녕하세요.");
+                JOptionPane.showMessageDialog(
+                        frame, // 다이얼로그가 실행될 부모 컴포넌트
+                        "안녕하세요?", // 다이얼로그에 표시될 메시지
+                        "메시지", // 다이얼로그의 타이틀
+                        JOptionPane.INFORMATION_MESSAGE, // 메시지 타입
+                        null // 아이콘
+                );
+            }
+        });
+        btnMsgDlg.setFont(new Font("D2Coding", Font.PLAIN, 28));
+        btnMsgDlg.setBounds(12, 10, 410, 64);
+        frame.getContentPane().add(btnMsgDlg);
+        
+        btnConfirmDlg = new JButton("Confirm Dialog");
+        btnConfirmDlg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                int result = JOptionPane.showConfirmDialog(frame, "삭제할까요?");
+                int result = JOptionPane.showConfirmDialog(
+                        frame, // 다이얼로그의 부모 컴포넌트
+                        "선택한 파일을 삭제할까요?", // 다이얼로그의 메시지
+                        "삭제 확인", // 다이얼로그의 타이틀(제목 표시줄)
+                        JOptionPane.YES_NO_OPTION, // 확인 옵션(YES-NO, YES-NO-CANCEL, ...)
+                        JOptionPane.QUESTION_MESSAGE, // 메시지 타입(-> 아이콘을 자동으로 설정)
+                        null // 아이콘
+                );
+                // showConfirmDialog의 리턴값: YES=0, NO=1, CANCEL=2, X(닫기)=-1
+                btnConfirmDlg.setText("Confirm=" + result);
+            }
+        });
+        btnConfirmDlg.setFont(new Font("D2Coding", Font.PLAIN, 28));
+        btnConfirmDlg.setBounds(12, 84, 410, 64);
+        frame.getContentPane().add(btnConfirmDlg);
+    }
+}
