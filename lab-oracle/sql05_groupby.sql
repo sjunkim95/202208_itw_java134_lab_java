daee1017a0e8206204b74c50785ea3809b06c880
JakeOh <jake.oh.lecture@gmail.com>
2022-09-30 18:08:30 +0900

    group by

--- lab-oracle/sql05_groupby.sql
+++ lab-oracle/sql05_groupby.sql
@@ -19,3 +19,42 @@ from emp;
 -- nvl2(컬럼, value1, value2): 컬럼이 null이 아니면 value1을 리턴하고, null이면 value2를 리턴.
 select comm, nvl(comm, -1), nvl2(comm, comm, -1)
 from emp;
+
+-- 다중행 함수(그룹 함수)
+-- count(컬럼): null이 아닌 행의 개수를 리턴.
+select count(empno), count(mgr), count(comm)
+from emp;
+
+select count(*) from emp; -- 테이블의 행 개수
+
+-- 통계 함수: 합계, 평균, 최댓값, 최솟값, 분산, 표준편차
+select sum(sal), avg(sal), max(sal), min(sal), variance(sal), stddev(sal)
+from emp;
+
+-- 그룹별 통계
+-- 부서(번호)별 급여의 최댓값.
+select max(sal) from emp where deptno = 10;
+select max(sal) from emp where deptno = 20;
+select max(sal) from emp where deptno = 30;
+
+select deptno, max(sal)
+from emp
+group by deptno
+order by deptno;
+
+-- 부서별 급여 평균
+select deptno, round(avg(sal), 2)
+from emp
+group by deptno
+order by deptno;
+
+-- 아래의 모든 문제에서 소수점은 반올림해서 2자리까지 표시.
+-- Ex. 부서별 급여의 평균, 표준편차를 출력. 부서번호 오름차순 정렬.
+
+-- Ex. 직무별 직원수, 급여의 최댓값, 최솟값, 평균 검색.
+-- 직무 오름차순 정렬
+
+-- Ex. 부서별, 직무별 부서번호, 직무, 직원수, 급여 평균 검색
+-- 정렬 순서는 (1) 부서 번호 (2) 직책 오름차순
+
+-- Ex. 입사연도별 사원수 검색. (힌트) to_char(날짜, 포맷) 이용.
