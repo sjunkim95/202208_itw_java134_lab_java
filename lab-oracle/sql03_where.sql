d2f45867ad72155d231d19c91c2eceee117abaad
JakeOh <jake.oh.lecture@gmail.com>
2022-09-30 14:21:12 +0900

    where

--- lab-oracle/sql03_where.sql
+++ lab-oracle/sql03_where.sql
@@ -50,13 +50,74 @@ order by deptno, ename;
 
 -- 직원 테이블에서 직무가 'CLERK'인 직원들의 직무, 이름, 급여를 출력.
 -- 이름 오름차순 순서로 출력
+select job, ename, sal
+from emp
+where job = 'CLERK'
+order by ename;
+-- SQL에서는 문자열 비교에서도 =, != 사용.
+-- 테이블에 저장된 데이터는 대/소문자를 구분. SQL 명령어는 대/소문자를 구분하지 않음.
+-- 문자열은 작은따옴표('')를 사용.
 
 -- 직원 테이블에서 직무가 CLERK 또는 MANAGER인 직원들의 직무, 이름, 급여를 검색.
 -- 출력 순서 (1) 직무 (2) 급여 오름차순 정렬.
+select job, ename, sal
+from emp
+where job = 'CLERK' or job = 'MANAGER'
+order by job, sal;
+
+select job, ename, sal
+from emp
+where job in ('CLERK', 'MANAGER')
+order by job, sal;
 
 -- 직원 테이블에서 20번 부서에서 근무하는 CLERK의 모든 정보를 검색
+select *
+from emp
+where deptno = 20 and job = 'CLERK';
 
 -- 직원 테이블에서 20번 부서에서 근무하거나 또는 직무가 CLERK인 직원들의 모든 정보를 검색
+select *
+from emp
+where deptno = 20 or job = 'CLERK';
 
 -- 직원 테이블에서 CLERK, ANALYST, MANAGER가 아닌 직원들의 사번, 이름, 직무, 급여를 검색
 -- 사번 오름차순 정렬.
+select empno, ename, job, sal
+from emp
+where job != 'CLERK' and job != 'ANALYST' and job != 'MANAGER'
+order by empno;
+
+select empno, ename, job, sal
+from emp
+where not (job = 'CLERK' or job = 'ANALYST' or job = 'MANAGER')
+order by empno;
+
+select empno, ename, job, sal
+from emp
+where job not in ('CLERK', 'ANALYST', 'MANAGER')
+order by empno;
+
+-- 숫자뿐 만 아니라 문자열(varchar2), 날짜(date, timestamp) 타입들도 대소 비교가 가능.
+
+-- 직원 테이블의 모든 레코드를 입사일의 오름차순으로 출력.
+select *
+from emp
+order by hiredate;
+
+-- '1987/01/01'을 포함해서 그 이후에 입사한 사원들의 레코드(모든 컬럼)을 검색.
+-- 입사일 오름차순으로 출력.
+select *
+from emp
+where hiredate >= '1987/01/01'  -- 문자열 타입과 날짜 타입을 같은 타입으로 변환 후에 대소 비교가 이루어짐.
+order by hiredate;
+
+-- 특정 문자열로 시작하거나, 특정 문자열이 포함된 값을 찾는 query.
+-- 'A'로 시작하는 이름을 갖는 직원들의 이름을 오름차순으로 출력.
+select ename
+from emp
+where ename like 'A%'  -- A로 시작하고, 그 뒤에는 글자수 제한이 없음.
+order by ename;
+
+select ename
+from emp
+where ename like 'A_';  -- A로 시작하고, 그 뒤에 한 글자만 더 있는 문자열.
