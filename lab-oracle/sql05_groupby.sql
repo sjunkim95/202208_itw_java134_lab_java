ed5eba478465806754bf75f83a07f45fc2e7c037
JakeOh <jake.oh.lecture@gmail.com>
2022-10-04 10:28:53 +0900

    group by

--- lab-oracle/sql05_groupby.sql
+++ lab-oracle/sql05_groupby.sql
@@ -50,11 +50,70 @@ order by deptno;
 
 -- 아래의 모든 문제에서 소수점은 반올림해서 2자리까지 표시.
 -- Ex. 부서별 급여의 평균, 표준편차를 출력. 부서번호 오름차순 정렬.
+select deptno, 
+    round(avg(sal), 2) as "AVG_SAL", 
+    round(stddev(sal), 2) as "STD_SAL"
+from emp
+group by  deptno
+order by deptno;
 
 -- Ex. 직무별 직원수, 급여의 최댓값, 최솟값, 평균 검색.
 -- 직무 오름차순 정렬
+select job, 
+    count(job) as "COUNT", 
+    max(sal) as "MAX_SAL", 
+    min(sal) as "MIN_SAL", 
+    round(avg(sal), 2) as "AVG_SAL"
+from emp
+group by job
+order by job;
 
 -- Ex. 부서별, 직무별 부서번호, 직무, 직원수, 급여 평균 검색
 -- 정렬 순서는 (1) 부서 번호 (2) 직책 오름차순
+select deptno, job, count(*), avg(sal)
+from emp
+group by deptno, job
+order by deptno, job;
 
 -- Ex. 입사연도별 사원수 검색. (힌트) to_char(날짜, 포맷) 이용.
+-- select to_char(hiredate, 'YYYY') from emp;
+select to_char(hiredate, 'YYYY') as "YEAR",
+    count(*) as "COUNT"
+from emp
+group by to_char(hiredate, 'YYYY')
+order by year;
+-- select 절에서 만든 별명(alias)는 where, group by, having 절에서는 사용할 수 없고,
+-- order by 절에서는 사용 가능.
+
+/*
+ * select 문장의 순서:
+ * select 컬럼, ...
+ * from 테이블
+ * where (그룹을 묶기 전에 검사할) 조건식
+ * group by 그룹을 묶어줄 컬럼, ...
+ * having 그룹을 묶은 다음에 검사할 조건식
+ * order by 출력 순서를 결정할 컬럼, ...;
+ */
+ 
+-- 부서별 부서번호, 급여 평균을 검색.
+-- 부서별 급여 평균이 2000 이상인 부서들만 출력.
+-- 부서 번호 오름차순 정렬.
+select deptno, avg(sal)
+from emp
+group by deptno
+having avg(sal) >= 2000
+order by deptno;
+
+-- Ex. emp 테이블에서 mgr가 null이 아닌 직원들 중에서
+-- 부서별 급여 평균을 검색. 부서 번호 오름차순 출력.
+
+-- Ex. 직책별 사원수를 검색.
+-- 직책이 PRESIDENT는 제외.
+-- 직책별 사원수가 3명 이상인 경우만 선택.
+-- 직책 오름차순 출력.
+
+-- Ex. 입사연도, 부서 번호, 입사연도별 부서별 입사한 사원수 검색.
+-- 1980년 제외.
+-- 연도별 부서별 사원수가 2명 이상인 경우 선택.
+-- 연도 오름차순 출력.
+
