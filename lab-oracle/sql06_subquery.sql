3e01ce49324efb854b5023ba449a05f0e4125080
JakeOh <jake.oh.lecture@gmail.com>
2022-10-04 14:21:31 +0900

    subquery

--- lab-oracle/sql06_subquery.sql
+++ lab-oracle/sql06_subquery.sql
@@ -47,3 +47,46 @@ where sal * 12 + nvl(comm, 0) > (
     where ename = 'WARD'
 )
 order by ANNUAL_SAL desc;
+
+-- Ex. SCOTT과 같은 급여를 받는 직원(들)의 이름과 급여를 검색.
+select ename, sal
+from emp
+where sal = (
+    select sal from emp where ename = 'SCOTT'
+);
+
+-- Ex. 위 결과에서 SCOTT은 제외하고 검색.
+select ename, sal
+from emp
+where sal = ( select sal from emp where ename = 'SCOTT' )
+    and ename != 'SCOTT';
+
+-- Ex. ALLEN보다 늦게 입사한 직원(들)의 이름, 입사날짜를 최근 입사일부터 출력.
+select ename, hiredate
+from emp
+where hiredate > (
+    select hiredate from emp where ename = 'ALLEN'
+);
+
+-- Ex. 매니저가 KING인 직원(들)의 사번, 이름, 매니저 사번을 검색.
+-- (힌트) 매니저 사번이 KING의 사번과 일치하는 레코드를 검색.
+select empno, ename, mgr
+from emp
+where mgr = (
+    select empno from emp where ename = 'KING'
+);
+
+-- Ex. DALLAS에서 근무하는 직원들의 이름, 급여, 부서번호를 검색.
+-- (힌트) dept 테이블에서 위치가 DALLAS인 부서의 부서 번호를 subquery로.
+select ename, sal, deptno
+from emp
+where deptno = (
+    select deptno from dept where loc = 'DALLAS'
+);
+
+-- 단일행 서브쿼리: 서브쿼리의 결과가 행이 1개 이하인 경우.
+-- 다중행 서브쿼리: 서브쿼리의 결과가 2개 이상의 행인 경우.
+--   다중행 서브쿼리에서는 한 개의 값과 단순비교(=, !=, >, <, ...)를 할 수 없음!
+--   in, any, all과 같은 키워드를 함께 사용해야 함.
+
+
