8787cfa70c3a96104852a468e6f79fc04f4d2ef5
JakeOh <jake.oh.lecture@gmail.com>
2022-10-04 17:32:36 +0900

    join

--- lab-oracle/sql07_join.sql
+++ lab-oracle/sql07_join.sql
@@ -10,4 +10,97 @@ join 문법:
     select 컬럼, ...
     from 테이블1, 테이블2, ...
     where join조건;
+    
+join 종류:
+1) inner join
+2) outer join
+   (1) left outer join
+   (2) right outer join
+   (3) full outer join
+join 종류를 말할 때 inner, outer는 생략 가능.
 */
+
+-- 사번, 이름, 부서번호, 부서이름 검색 - inner join
+-- 1) ANSI 표준
+select emp.empno, emp.ename, emp.deptno, dept.dname
+from emp
+    inner join dept on emp.deptno = dept.deptno;
+    
+-- 2) Oracle:
+select emp.empno, emp.ename, emp.deptno, dept.dname
+from emp, dept
+where emp.deptno = dept.deptno;
+
+-- inner join은 두 테이블(emp, dept)에서 공통으로 등장하는 부서번호(10, 20, 30)가 검색됨.
+
+-- left/right/full outer join의 차이점:
+insert into emp (empno, ename, deptno)
+values (1004, '오쌤', 50);
+
+commit; -- 테이블 변경 내용을 영구 저장.
+
+-- 사번, 이름, 부서번호, 부서이름 - left outer join
+-- 왼쪽 테이블에서 등장하는 모든 부서번호(10, 20, 30, 50)가 검색됨.
+-- 1) ANSI:
+select e.empno, e.ename,  e.deptno, d.deptno, d.dname
+from emp e
+    left outer join dept d on e.deptno = d.deptno;
+
+-- 2) Oracle:
+select e.empno, e.ename, e.deptno, d.dname
+from emp e, dept d
+where e.deptno = d.deptno(+);
+
+-- 사번, 이름, 부서번호, 부서이름 - right outer join
+-- 오른쪽 테이블(dept)에 등장하는 모든 부서번호(10, 20, 30, 40)가 검색됨.
+-- 1) ANSI:
+select e.empno, e.ename, e.deptno, d.deptno, d.dname
+from emp e
+    right outer join dept d on e.deptno = d.deptno;
+
+-- 2) Oracle:
+select e.empno, e.ename, e.deptno, d.deptno, d.dname
+from emp e, dept d 
+where e.deptno(+) = d.deptno;
+    
+-- 사번, 이름, 부서번호, 부서이름 - full outer join
+-- 두 테이블(emp, dept) 중 적어도 한 곳에 등장하는 모든 부서번호(10, 20, 30, 40, 50)가 검색됨.
+-- 1) ANSI:
+select e.empno, e.ename, e.deptno, d.deptno, d.dname
+from emp e
+    full outer join dept d on e.deptno = d.deptno;
+
+-- 2) Oracle: full outer join의 문법을 제공하지 않음. 합집합 연산을 이용할 수 있음.
+select e.empno, e.ename, e.deptno, d.deptno, d.dname
+from emp e, dept d
+where e.deptno = d.deptno(+)  -- left join
+union -- 합집합
+select e.empno, e.ename, e.deptno, d.deptno, d.dname
+from emp e, dept d
+where e.deptno(+) = d.deptno; -- right join
+
+-- 사번, 부서이름, 이름, 급여를 검색(inner join).
+-- 급여가 2000 이상인 직원들을 검색. 출력 순서는 급여의 내림차순.
+-- 1) ANSI:
+select e.empno, d.dname, e.ename, e.sal
+from emp e
+    join dept d on e.deptno = d.deptno
+where e.sal >= 2000
+order by e.sal desc;
+
+-- 2) Oracle:
+select e.empno, d.dname, e.ename, e.sal
+from emp e, dept d 
+where e.deptno = d.deptno
+    and e.sal >= 2000
+order by e.sal desc;
+
+-- self-join: 같은 테이블에서 join을 하는 것.
+-- 사번, 이름, 매니저 사번, 매니저 이름을 검색
+select e1.empno, e1.ename as "직원 이름", e1.mgr, e2.ename as "매니저 이름"
+from emp e1
+    join emp e2 on e1.mgr = e2.empno;
+
+select e1.empno, e1.ename as "직원 이름", e1.mgr, e2.ename as "매니저 이름"
+from emp e1, emp e2
+where e1.mgr = e2.empno;
