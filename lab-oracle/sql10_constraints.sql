46cb11adfac4a9afa91dfc23394489656af94727
JakeOh <jake.oh.lecture@gmail.com>
2022-10-05 17:15:48 +0900

    constraints

--- lab-oracle/sql10_constraints.sql
+++ lab-oracle/sql10_constraints.sql
@@ -33,9 +33,19 @@ insert into ex01 (col_2) values ('홍길동');
 insert into ex01 (col_1) values (2);
 --> 에러 발생: col_2는 NN이기 때문에 NULL이 될 수 없음!
 
+insert into ex01 (col_1, col_2, col_3) values (2, '홍길동', 'test');
+--> 에러 발생: col_3은 unique 제약 조건인데, 기존에 입력된 값과 같은 'test'를 insert하려고 했기 때문에.
 
+insert into ex01 (col_1, col_2) values (2, '홍길동');
+--> 성공: PK, NN 만족.
 
+insert into ex01 (col_1, col_2, col_4) values (3, '홍길동', -1);
+--> 에러 발생: col_4 >= 0 (check 제약조건) 이어야 하는데, -1을 insert하려고 했기 때문에.
 
+insert into ex01 (col_1, col_2, col_4) values (3, '홍길동', 0);
+--> 성공
 
+select * from ex01;
 
+commit;
 
