551f6312b4984cb1eb0358deaf6efa689ce7b5ee
Jake Oh <jake.oh.lecture@gmail.com>
2022-10-07 17:31:05 +0900

    JDBC, Swing, Blog

--- lab17_jdbc/src/edu/java/ojdbc/controller/BlogDaoImpl.java
+++ lab17_jdbc/src/edu/java/ojdbc/controller/BlogDaoImpl.java
@@ -1,9 +1,19 @@
 package edu.java.ojdbc.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.java.ojdbc.model.Blog;
+import oracle.jdbc.driver.OracleDriver;
 
+import static edu.java.ojdbc.OracleJdbc.*;
+import static edu.java.ojdbc.controller.JdbcSql.*;
 import static edu.java.ojdbc.model.Blog.Entity.*;
 
 public class BlogDaoImpl implements BlogDao {
@@ -23,8 +33,44 @@ public class BlogDaoImpl implements BlogDao {
 
     @Override
     public List<Blog> select() {
-        // TODO Auto-generated method stub
-        return null;
+        List<Blog> list = new ArrayList<>();
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            DriverManager.registerDriver(new OracleDriver());
+            
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            
+            stmt = conn.prepareStatement(SQL_SELECT_ALL);
+            
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                Integer blogNo = rs.getInt(COL_BLOG_NO);
+                String title = rs.getString(COL_TITLE);
+                String content = rs.getString(COL_CONTENT);
+                String author = rs.getString(COL_AUTHOR);
+                LocalDateTime created = rs.getTimestamp(COL_CREATED_DATE).toLocalDateTime();
+                LocalDateTime modified = rs.getTimestamp(COL_MODIFIED_DATE).toLocalDateTime();
+                
+                Blog blog = new Blog(blogNo, title, content, author, created, modified);
+                list.add(blog);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+                stmt.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return list;
     }
 
     @Override
