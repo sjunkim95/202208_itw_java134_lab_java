0e0344300aa7fd50fa6f56798c57db9b8c15490d
Jake Oh <jake.oh.lecture@gmail.com>
2022-10-07 16:26:01 +0900

    JDBC

---
+++ lab17_jdbc/src/edu/java/ojdbc/controller/BlogDaoImpl.java
@@ -0,0 +1,41 @@
+package edu.java.ojdbc.controller;
+
+import java.util.List;
+
+import edu.java.ojdbc.model.Blog;
+
+import static edu.java.ojdbc.model.Blog.Entity.*;
+
+public class BlogDaoImpl implements BlogDao {
+
+    @Override
+    public List<Blog> select() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Blog select(Integer blogNo) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int insert(Blog blog) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int update(Blog blog) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int delete(Integer blogNo) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
