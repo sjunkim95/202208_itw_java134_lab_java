64eadad498a39ae50daf845fa66f93dcacb60cbc
Jake Oh <jake.oh.lecture@gmail.com>
2022-10-12 16:51:57 +0900

    contact version 0.6

--- lab_contact/src/edu/java/contact/ver06/ContactDao.java
+++ lab_contact/src/edu/java/contact/ver06/ContactDao.java
@@ -7,6 +7,7 @@ public interface ContactDao {
     
     List<Contact> read(); // 연락처 전체 검색
     Contact read(Integer cid); // 연락처 테이블의 고유키(PK)로 검색
+    List<Contact> read(String keyword);
     int create(Contact contact); // insert 문장 실행
     int update(Contact contact); // update 문장 실행
     int delete(Integer cid); // delete 문장
