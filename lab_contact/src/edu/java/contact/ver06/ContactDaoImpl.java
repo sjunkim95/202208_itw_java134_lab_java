89c57c6da76bf81f436afde5956245a19a7f1b99
Jake Oh <jake.oh.lecture@gmail.com>
2022-10-12 14:25:53 +0900

    JDBC, swing, contacts

---
+++ lab_contact/src/edu/java/contact/ver06/ContactDaoImpl.java
@@ -0,0 +1,37 @@
+package edu.java.contact.ver06;
+
+import java.util.List;
+
+public class ContactDaoImpl implements ContactDao {
+
+    @Override
+    public List<Contact> read() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Contact read(Integer cid) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int create(Contact contact) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int update(Contact contact) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int delete(Integer cid) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ lab_contact/src/edu/java/contact/ver06/ContactSql.java
@@ -0,0 +1,18 @@
+package edu.java.contact.ver06;
+
+public interface ContactSql {
+    
+    // TODO: ContactDao.read() 메서드에서 사용할 SQL
+    // select * from CONTACTS order by NAME
+    
+    // TODO: ContactDao.read(Integer cid) 메서드에서 사용할 SQL
+    // select * from CONTACTS where CID = ?
+    
+    // TODO: ContactDao.create(Contact contact) 메서드에서 사용할 SQL
+    
+    // TODO: ContactDao.update(Contact contact) 메서드에서 사용할 SQL
+    
+    // TODO: ContactDao.delete(Integer cid) 메서드에서 사용할 SQL
+    // delete from CONTACTS where CID = ?
+
+}
